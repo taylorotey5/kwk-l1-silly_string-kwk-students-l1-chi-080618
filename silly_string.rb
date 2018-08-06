0439c4e4ac1c6000652216f4f21e849cd25518df
Taylor Otey <taylorotey5@gmail.com>
2018-08-06 20:13:20 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -3,7 +3,7 @@
 
 your_favorite_word = "happy"
 
-puts "My favorite word is #{happy}"
+puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
